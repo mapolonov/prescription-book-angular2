286a9df2b3be1cf40f3576348c025403b7207599
mapolonov <mykhailo.apolonov@globallogic.com>
2019-05-05 17:32:43 +0300

    add details

--- src/app/order-list/order-list.component.ts
+++ src/app/order-list/order-list.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { RepairAction } from '../shared/repair-action.model';
 
 @Component({
   selector: 'app-order-list',
@@ -6,6 +7,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./order-list.component.sass']
 })
 export class OrderListComponent implements OnInit {
+
+  repairActions: RepairAction[] = [
+    new RepairAction('Checking', 1, 30),
+    new RepairAction('Export new mirror', 7, 500)
+  ];
 
   constructor() { }
 
---
+++ src/app/shared/repair-action.model.ts
@@ -0,0 +1,5 @@
+export class RepairAction {
+    constructor(public name: string,
+                public time: number,
+                public cost: number) {}
+}
