b4dd291664d8f3b356f57c1c8e44290a7afccee2
mapolonov <mykhailo.apolonov@globallogic.com>
2019-05-05 18:22:57 +0300

    implement event binding

--- src/app/breakdowns/breakdown-list/breakdown-item/breakdown-item.component.ts
+++ src/app/breakdowns/breakdown-list/breakdown-item/breakdown-item.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
+import { Breakdown } from '../breakdown.model';
 
 @Component({
   selector: 'app-breakdown-item',
@@ -6,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./breakdown-item.component.sass']
 })
 export class BreakdownItemComponent implements OnInit {
+  @Input() breakdown: Breakdown;
 
   constructor() { }
 
